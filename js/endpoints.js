457a2bb411eee1eddd1a0418e4a304a9c9af8424
Marco Ramirez <marco.ramirez@ugto.mx>
2024-02-06 09:52:26 -0600

    fixes

--- js/endpoints.js
+++ js/endpoints.js
@@ -1,18 +1,19 @@
 const search = async (business) => {
-  const url = `https://local-business-data.p.rapidapi.com/search?query=${business}&limit=10&language=en`;
+	/*
+  	const url = `https://local-business-data.p.rapidapi.com/search?query=${business}&limit=50&language=en`;
 	const options = {
 	method: 'GET',
 	headers: {
-			'X-RapidAPI-Key': '7660a54fbdmshed55202c4f5aecbp115c75jsnbe50b5035956',
+			'X-RapidAPI-Key': 'd599a4c174msh32f4ea959f89407p1746c1jsn5bda765a684d',
 			'X-RapidAPI-Host': 'local-business-data.p.rapidapi.com'
 		}
 	};
-
+	*/
 	try {
-		const response = await fetch(url, options);
+		const url = '../api/business.json'
+		const response = await fetch(url);
 		const result = await response.json();
-		return result.data
-		// console.log('@@ result search => ', result);
+		return result
 	} catch (error) {
 		console.error(error);
 	}
